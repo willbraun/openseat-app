2a098eaf278644ba31738a793e3ea54279164b84
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-11 17:55:53 -0400

    search bar has been added to discover page

--- frontend/static/src/components/Search.js
+++ frontend/static/src/components/Search.js
@@ -11,39 +11,29 @@ const Search = ({currentSearch, currentRadius, findEvents}) => {
 
     return (
         <div className="search-bar">
-            <Form onSubmit={() => findEvents(value.label, radius)}>
-                <Row className="search-bar-row">
-                    <Col xs={6}>
-                        <Form.Group className="location-search-input">
-                            <GooglePlacesAutocomplete selectProps={{
-                                value,
-                                onChange: setValue,
-                                placeholder: currentSearch,
-                            }} />
-                        </Form.Group>
-                    </Col>
-                    <Col xs={1}>
-                        <p>Within</p>
-                    </Col>
-                    <Col xs={2}>
-                        <Form.Group >
-                            <Form.Select
-                                name="radius" 
-                                value={radius}
-                                onChange={(e) => setRadius(e.target.value)}>
-                                <option value="2">2 miles</option>
-                                <option value="5">5 miles</option>
-                                <option value="10">10 miles</option>
-                                <option value="25">25 miles</option>
-                                <option value="50">50 miles</option>
-                                <option value="100">100 miles</option>
-                            </Form.Select>
-                        </Form.Group>
-                    </Col>
-                    <Col xs={3}>
-                        <button className="search-submit" type="submit">Find Events</button>
-                    </Col>
-                </Row>
+            <Form className="search-bar-form" onSubmit={() => findEvents(value.label, radius)}>
+                <Form.Group className="location-search-input">
+                    <GooglePlacesAutocomplete selectProps={{
+                        value,
+                        onChange: setValue,
+                        placeholder: currentSearch,
+                    }} />
+                </Form.Group>
+                <p>within</p>
+                <Form.Group >
+                    <Form.Select
+                        name="radius" 
+                        value={radius}
+                        onChange={(e) => setRadius(e.target.value)}>
+                        <option value="2">2 miles</option>
+                        <option value="5">5 miles</option>
+                        <option value="10">10 miles</option>
+                        <option value="25">25 miles</option>
+                        <option value="50">50 miles</option>
+                        <option value="100">100 miles</option>
+                    </Form.Select>
+                </Form.Group>
+                <button className="search-submit" type="submit">Find Events</button>
             </Form>
         </div>
         
