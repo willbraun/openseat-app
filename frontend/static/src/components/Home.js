0627cb95b9fe9eef2812c455be805926ddc91914
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-11 17:15:20 -0400

    started formatting search bar on discover page

--- frontend/static/src/components/Home.js
+++ frontend/static/src/components/Home.js
@@ -11,13 +11,13 @@ import './../styles/eventlist.css';
 
 const Home = ({appState, setAppState}) => {
     const currentSearch = window.localStorage.openSeatSearchLocation ?? 'Greenville, SC, USA';
+    const currentRadius = ['2', '5', '10', '25', '50', '100'].includes(window.localStorage.openSeatSearchRadius) ? window.localStorage.openSeatSearchRadius : '10';
    
     // const [radius, setRadius] = useState(10);
     
     const [state, setState] = useState({
         // location: appState.userZip.length > 0 ? appState.userZip : '29601',
         // location: defaultSearch,
-        radius: 20,
         events: null,
     })
 
@@ -26,8 +26,8 @@ const Home = ({appState, setAppState}) => {
 
     // update origin_zip to origin in backend
     
-    const getHomeEvents = async (searchTerm) => {
-        const response = await fetch(`/api_v1/events/?origin_zip=${searchTerm}&radius=${state.radius}`).catch(handleError);
+    const getHomeEvents = async (searchLocation, searchRadius) => {
+        const response = await fetch(`/api_v1/events/?origin_zip=${searchLocation}&radius=${searchRadius}`).catch(handleError);
         
         if (!response.ok) {
             throw new Error('Network response was not ok!');
@@ -38,20 +38,24 @@ const Home = ({appState, setAppState}) => {
     }
 
     useEffect(() => {
-        getHomeEvents(currentSearch);
+        getHomeEvents(currentSearch, currentRadius);
     }, [location.key])
 
-    const findEvents = (searchTerm) => {
+    const findEvents = (searchLocation, searchRadius) => {
         setState({...state, events: null});
-        getHomeEvents(searchTerm);
-        localStorage.setItem('openSeatSearchLocation', searchTerm);
+        getHomeEvents(searchLocation, searchRadius);
+        localStorage.setItem('openSeatSearchLocation', searchLocation);
+        localStorage.setItem('openSeatSearchRadius', searchRadius);
     }
 
     const noneFound = `No events found. ${appState.auth ? "Create one!" : "Log in to create one!"}`;
     
     return (
         <main className="home-page">
-            <Search currentSearch={currentSearch} findEvents={findEvents}/>
+            <Search 
+                currentSearch={currentSearch} 
+                currentRadius={currentRadius}
+                findEvents={findEvents}/>
 
             {state.events === null ? 
                 <div>Loading events...</div> : 
--- frontend/static/src/components/Search.js
+++ frontend/static/src/components/Search.js
@@ -1,21 +1,52 @@
 import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import './../styles/search.css';
 
-const Search = ({currentSearch, findEvents}) => {
+const Search = ({currentSearch, currentRadius, findEvents}) => {
     const [value, setValue] = useState(null);
+    const [radius, setRadius] = useState(currentRadius);
 
     return (
-        <div>
-            <p>Events near</p>
-            <GooglePlacesAutocomplete selectProps={{
-                value,
-                onChange: setValue,
-                placeholder: currentSearch,
-            }} />
-            <p>within</p>
-            
-            <button type="button" onClick={() => findEvents(value.label)}>Find Events</button>
+        <div className="search-bar">
+            <Form onSubmit={() => findEvents(value.label, radius)}>
+                <Row className="search-bar-row">
+                    <Col xs={6}>
+                        <Form.Group className="location-search-input">
+                            <GooglePlacesAutocomplete selectProps={{
+                                value,
+                                onChange: setValue,
+                                placeholder: currentSearch,
+                            }} />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={1}>
+                        <p>Within</p>
+                    </Col>
+                    <Col xs={2}>
+                        <Form.Group >
+                            <Form.Select
+                                name="radius" 
+                                value={radius}
+                                onChange={(e) => setRadius(e.target.value)}>
+                                <option value="2">2 miles</option>
+                                <option value="5">5 miles</option>
+                                <option value="10">10 miles</option>
+                                <option value="25">25 miles</option>
+                                <option value="50">50 miles</option>
+                                <option value="100">100 miles</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                    <Col xs={3}>
+                        <button className="search-submit" type="submit">Find Events</button>
+                    </Col>
+                </Row>
+            </Form>
         </div>
+        
     )
 }
 
