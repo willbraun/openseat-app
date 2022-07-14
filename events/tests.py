939526d1752de3588bbf4f6bd13cb9150fe978d8
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-07-14 11:10:02 -0400

    added tests for endpoints in django

--- events/tests.py
+++ events/tests.py
@@ -18,24 +18,6 @@ class EventListTestCases(TestCase):
         response = client.get(reverse('api:events:events'), {'origin': '29615', 'radius': '50'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_my_seats_future_no_auth(self):
-        response = client.get(reverse('api:events:my-seats-future')) 
-        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_my_seats_past_no_auth(self):
-        response = client.get(reverse('api:events:my-seats-past')) 
-        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_get_my_events_no_auth(self):
-    #     response = client.get(reverse('api:events:my-events')) 
-    #     self.assertNotEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_get_my_events_no_auth(self):
-    #     request = factory.get('/api_v1/events/mine/')
-    #     view = MyEventsListCreateApiView.as_view()
-    #     response = view(request)
-    #     self.assertNotEqual(response.status_code, status.HTTP_200_OK)
-
     def test_get_discover_events_auth(self):
         request = factory.get('/api_v1/events/', {'origin': '29615', 'radius': '50'})
         user = User.objects.create(username='test_user')
@@ -103,6 +85,7 @@ class FillSeatsEventTestCases(TestCase):
         response = view(request, pk=id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
     def test_remove_self_no_auth(self):
         id = Event.objects.get(name='Fishing').id
         request = factory.put(f'/api_v1/{id}/remove-self/')
@@ -123,7 +106,7 @@ class FillSeatsEventTestCases(TestCase):
 class EventCrudTestCases(TestCase):
 
     def setUp(self):
-        creator1 = User.objects.create(username='test_creator1')
+        creator = User.objects.create(username='test_creator')
 
         Event.objects.create(
             name='Go-karting',
@@ -135,55 +118,74 @@ class EventCrudTestCases(TestCase):
             zip_code='12345',
             date='2100-01-01',
             time='12:00:00',
-            creator=creator1,
+            creator=creator,
         )
     
-    # def test_create_event_auth(self):
-    #     creator2 = User.objects.create(username='test_creator2')
-    #     user = User.objects.create(username='test_user')
+    def test_create_event_auth(self):
+
+        def temporary_image():
+            import tempfile
+            from PIL import Image
+
+            image = Image.new('RGB', (100,100))
+            tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+            image.save(tmp_file, 'jpeg')
+            tmp_file.seek(0)
+            return tmp_file
+
+        user = User.objects.create(username='test_user')
+        image = temporary_image()
+
+        request = factory.post('/api_v1/events/mine/', ({
+            'name': 'Dancing',
+            'description': 'Lets go dancing',
+            'seats': 4,
+            'image': image,
+            'address': '1234 testing lane',
+            'city': 'Greenville',
+            'state': 'SC',
+            'zip_code': '12345',
+            'date': '2100-01-01',
+            'time': '12:00:00',
+            }))
+
+        view = MyEventsListCreateApiView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        response.render()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(user.id, response.data['creator']['id'])
+        self.assertEqual(user.id, response.data['participants'][0]['id'])
         
-    #     request = factory.post('/api_v1/events/mine/', ({
-    #         'name': 'Dancing',
-    #         'description': 'Lets go dancing',
-    #         'seats': 4,
-    #         'address': '1234 testing lane',
-    #         'city': 'Greenville',
-    #         'state': 'SC',
-    #         'zip_code': '12345',
-    #         'date': '2100-01-01',
-    #         'time': '12:00:00',
-    #         'creator': creator2,
-    #         }))
-    #         # include image and maybe other fields
-    #     view = MyEventsListCreateApiView.as_view()
-    #     force_authenticate(request, user=user)
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+        saved_location = f"media/events/images/{image.name.split('/')[-1]}"
+        import os
+        os.remove(saved_location)
+        
 
     def test_read_event_auth(self):
         id = Event.objects.get(name='Go-karting').id
-        creator1 = User.objects.get(username='test_creator1')
+        creator = User.objects.get(username='test_creator')
         request = factory.get(f'/api_v1/events/{id}/')
         view = EventDetailApiView.as_view()
-        force_authenticate(request, user=creator1)
+        force_authenticate(request, user=creator)
         response = view(request, pk=id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_edit_event_auth(self):
         id = Event.objects.get(name='Go-karting').id
-        creator1 = User.objects.get(username='test_creator1')
+        creator = User.objects.get(username='test_creator')
         request = factory.patch(f'/api_v1/events/{id}/', {'name': 'Mario-karting'})
         view = EventDetailApiView.as_view()
-        force_authenticate(request, user=creator1)
+        force_authenticate(request, user=creator)
         response = view(request, pk=id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_event_auth(self):
         id = Event.objects.get(name='Go-karting').id
-        creator1 = User.objects.get(username='test_creator1')
+        creator = User.objects.get(username='test_creator')
         request = factory.delete(f'/api_v1/events/{id}/')
         view = EventDetailApiView.as_view()
-        force_authenticate(request, user=creator1)
+        force_authenticate(request, user=creator)
         response = view(request, pk=id)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
